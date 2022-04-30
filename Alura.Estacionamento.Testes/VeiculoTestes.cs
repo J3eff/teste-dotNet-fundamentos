11c5dc7339f84165ae1f7bf2b2dee5ed41dd599e
J3eff <jeffersonvideo125@gmail.com>
2022-04-30 13:41:56 -0300

    Utilizando xUnit

---
+++ Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -0,0 +1,42 @@
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            //Act
+            veiculo.Acelerar(10);
+            //Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            //Arange
+            var veiculo = new Veiculo();
+            //Act
+            veiculo.Frear(10);
+            //Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestarTipoVeiculo()
+        {
+            var veiculo = new Veiculo();
+
+            veiculo.Tipo = TipoVeiculo.Motocicleta;
+
+            Assert.Equal(TipoVeiculo.Motocicleta, veiculo.Tipo);
+        }
+    }
+}
