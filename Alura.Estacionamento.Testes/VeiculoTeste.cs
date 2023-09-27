26328ead9110dcc7aed03539aa54e67771bd6c54
J3eff <jeffersonvideo125@gmail.com>
2023-09-26 23:06:25 -0300

    Testando exceções

--- Alura.Estacionamento.Testes/VeiculoTeste.cs
+++ Alura.Estacionamento.Testes/VeiculoTeste.cs
@@ -11,8 +11,9 @@ namespace Alura.Estacionamento.Testes
         private Veiculo veiculo;
         public ITestOutputHelper saidaConsoleTeste;
 
-        public VeiculoTeste(ITestOutputHelper _saidaConsoleTeste) { 
-            
+        public VeiculoTeste(ITestOutputHelper _saidaConsoleTeste)
+        {
+
             saidaConsoleTeste = _saidaConsoleTeste;
             saidaConsoleTeste.WriteLine("Construtor invocado.");
             veiculo = new Veiculo();
@@ -65,6 +66,34 @@ namespace Alura.Estacionamento.Testes
         [Fact(Skip = "Teste ainda não implementado")]
         public void ValidaNomeProprietarioDoVeiculo() { }
 
+        [Fact]
+        public void TestaNomeProprietarioVeiculoComMenosDeTresCaracteres()
+        {
+            //Arrange
+            string nomeProprietario = "Ab";
+
+            //Assert
+            Assert.Throws<System.FormatException>(
+                //Act
+                () => new Veiculo(nomeProprietario)
+            );
+        }
+
+        [Fact]
+        public void TestaMensagemDeExecaoDoQuartoCaractereDaPlaca()
+        {
+            //Arrange
+            string placa = "ASDF8888";
+
+            //Act
+            var mensagem = Assert.Throws<System.FormatException>(
+                () => new Veiculo().Placa = placa
+            );
+
+            //Assert
+            Assert.Equal("O 4° caractere deve ser um hífen", mensagem.Message);
+        }
+       
         public void Dispose()
         {
             saidaConsoleTeste.WriteLine("Dispose invocado.");
